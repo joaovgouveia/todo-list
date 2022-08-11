c7168197a85d7f9c3ad3cf0acf76816862750ae7
Jota <joaovictor.shark@gmail.com>
2022-08-10 22:31:51 -0300

    Todo list

---
+++ script.js
@@ -0,0 +1,37 @@
+//Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+//Event Listeners
+todoButton.addEventListener('click', AddTodo);
+
+//Functions
+function AddTodo(event){
+    event.preventDefault();
+
+    //Creating the Todo DIV
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add("todo");
+
+    //LI
+    const newTodo = document.createElement("li");
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
+
+    //Complete BUTTON
+    const checkButton = document.createElement("button");
+    checkButton.innerHTML = '<i class="fas fa-check"></i>';
+    checkButton.classList.add("check-btn");
+    todoDiv.appendChild(checkButton);
+
+    //Delete BUTTON
+    const deleteButton = document.createElement("button");
+    deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
+    deleteButton.classList.add("delete-btn");
+    todoDiv.appendChild(deleteButton);
+
+    todoList.appendChild(todoDiv);
+    todoInput.value = "";
+}
